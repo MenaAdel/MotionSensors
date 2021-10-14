a0fbb98945fbb40836bf659d31cf9589a08b2a09
m.adel <m.adel@t2.sa>
2021-10-14 12:21:54 +0200

    Add touch event

--- app/src/main/java/com/t2/motionsensors/MainActivity.kt
+++ app/src/main/java/com/t2/motionsensors/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.t2.motionsensors
 
+import android.content.res.Configuration
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
@@ -8,33 +9,78 @@ import android.icu.text.SimpleDateFormat
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.view.MotionEvent
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.gson.Gson
-import com.google.gson.JsonObject
-import com.t2.motionsensors.domain.entity.Coordinates
-import com.t2.motionsensors.domain.entity.SensorBody
+import com.t2.motionsensors.databinding.ActivityMainBinding
+import com.t2.motionsensors.domain.entity.*
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import java.util.*
+import kotlin.math.abs
 
+@RequiresApi(Build.VERSION_CODES.N)
 class MainActivity : AppCompatActivity(), SensorEventListener {
 
+    private lateinit var binding: ActivityMainBinding
+
     private var accelerometer: Sensor? = null
+    private var accelerometerUncalibrated: Sensor? = null
     private var gyroscope: Sensor? = null
     private var magnetometer: Sensor? = null
+    private var rotation: Sensor? = null
     private val sensorData = SensorBody()
     private val accelerometerArray = mutableListOf<Coordinates>()
     private val gyroscopeArray = mutableListOf<Coordinates>()
     private val magnetometerArray = mutableListOf<Coordinates>()
+    private val deviceMotionArray = mutableListOf<DeviceMotion>()
+    private var deviceMotionObject: DeviceMotion = DeviceMotion()
+    private var index = 0
+    private var startX: Float = 0f
+    private var endX: Float = 0f
+    private var startY: Float = 0f
+    private var endY: Float = 0f
+    private var moveX: Float = 0f
+    private var moveY: Float = 0f
+    private var startTime: Long = 0L
+    private var endTime: Long = 0L
+    private var tap: MutableList<Movement> = mutableListOf()
+    private var swipe: MutableList<Movement> = mutableListOf()
+    private var touchData: MutableList<Data> = mutableListOf()
+    private var touchSwipeData: MutableList<Data> = mutableListOf()
+    private var touchBody: TouchBody? = null
+    private val dateFormat: SimpleDateFormat by lazy {
+        SimpleDateFormat("yyyy-mm-dd HH:mm:ss:SSS",
+            Locale.getDefault())
+    }
+    private var isActionMove = false
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initRecycler()
         setupSensors()
+        touchBody = TouchBody(user_id = "test", swipe = swipe, tap = tap)
     }
 
+    private fun initRecycler() {
+        val data = ArrayList<ItemsViewModel>()
+
+        for (i in 1..20) {
+            data.add(ItemsViewModel(R.drawable.ic_launcher_background, "Item $i"))
+        }
+        with(binding.recyclerview) {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = CustomAdapter(data)
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
     private fun setupSensors() {
         val sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
 
@@ -55,6 +101,19 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         magnetometer?.let {
             sensorManager.registerListener(this, it, SensorManager.SENSOR_DELAY_NORMAL)
         } ?: Log.d(TAG, "magnetometer not supported")
+
+        //accelerometerUncalibrated
+        accelerometerUncalibrated =
+            sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER_UNCALIBRATED)
+        accelerometerUncalibrated?.let {
+            sensorManager.registerListener(this, it, SensorManager.SENSOR_DELAY_NORMAL)
+        } ?: Log.d(TAG, "accelerometerUncalibrated not supported")
+
+        //rotation
+        rotation = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR)
+        rotation?.let {
+            sensorManager.registerListener(this, it, SensorManager.SENSOR_DELAY_NORMAL)
+        } ?: Log.d(TAG, "rotation not supported")
     }
 
     companion object {
@@ -64,7 +123,6 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     @RequiresApi(Build.VERSION_CODES.N)
     override fun onSensorChanged(event: SensorEvent?) {
         Log.d(TAG, "sensor type is: ${event?.sensor?.type}")
-        val dateFormat = SimpleDateFormat("yyyy-mm-dd:HH:mm:ss", Locale.getDefault())
         when (event?.sensor?.type) {
             Sensor.TYPE_ACCELEROMETER -> {
                 accelerometerArray.add(
@@ -75,6 +133,15 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
                         time = dateFormat.format(Date())
                     )
                 )
+                deviceMotionObject.apply {
+                    accelerationIncludingGravity = CoordinatesDevice(
+                        x = event.values?.get(0),
+                        y = event.values?.get(1),
+                        z = event.values?.get(2)
+                    )
+                    orientation = getDeviceOrientation()
+
+                }
                 Log.d(
                     TAG,
                     "onSensorChanged x: ${event.values?.get(0)} y: ${event.values?.get(1)} z: ${
@@ -114,7 +181,23 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
                         event.values?.get(2)
                     }"
                 )
-
+            }
+            Sensor.TYPE_ACCELEROMETER_UNCALIBRATED -> {
+                deviceMotionObject.acceleration = CoordinatesDevice(
+                    x = event.values?.get(0),
+                    y = event.values?.get(1),
+                    z = event.values?.get(2)
+                )
+            }
+            Sensor.TYPE_ROTATION_VECTOR -> {
+                deviceMotionObject.apply {
+                    rotation = Rotation(
+                        alpha = event.values?.get(0),
+                        beta = event.values?.get(1),
+                        gamma = event.values?.get(2)
+                    )
+                    time = dateFormat.format(Date())
+                }
             }
             else -> {
             }
@@ -127,13 +210,125 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     }
 
     private suspend fun fillSensorData() {
+        delay(10000)
+        deviceMotionArray.add(index, deviceMotionObject)
+        deviceMotionObject = DeviceMotion()
+        index++
         delay(60000)
         sensorData.apply {
             accelerometer = accelerometerArray
             gyroscope = gyroscopeArray
             magnetometer = magnetometerArray
+            deviceMotion = deviceMotionArray
         }
         val jsonData = Gson().toJson(sensorData)
-        Log.d("json data is: " , jsonData)
+        // Log.d("json data is: ", jsonData)
+    }
+
+    private fun getDeviceOrientation(): Int {
+        return if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            1
+        } else {
+            0
+        }
+    }
+
+    override fun dispatchTouchEvent(event: MotionEvent?): Boolean {
+        Log.d(TAG, "finger area is ${event?.size}")
+        Log.d(TAG, "pressure is ${event?.pressure}")
+        for (i in 0 until (event?.pointerCount ?: 0)){
+            Log.d(TAG, "size of pointerCount $i is ${event?.getSize(i)}")
+        }
+        when (event?.action) {
+            MotionEvent.ACTION_DOWN -> {
+                startX = event.x
+                startY = event.y
+                startTime = System.currentTimeMillis()
+                Log.d(TAG, "startPoint is ${event.x} ,${event.y}")
+            }
+            MotionEvent.ACTION_MOVE -> {
+                isActionMove = true
+                moveX = event.x
+                moveY = event.y
+            }
+            MotionEvent.ACTION_UP -> {
+                onEventUp(event)
+            }
+            else -> {
+            }
+        }
+        return true
+    }
+
+    private fun onEventUp(event: MotionEvent) {
+        endX = event.x
+        endY = event.y
+        endTime = System.currentTimeMillis()
+        Log.d(TAG, "endPoint is ${event.x} ,${event.y}")
+        Log.d(TAG, "distance x is ${endX - startX}")
+        Log.d(TAG, "distance y is ${endY - startY}")
+        val xVelocity = calculateVelocity(startX, endX, (endTime - startTime).toFloat())
+        val yVelocity = calculateVelocity(startY, endY, (endTime - startTime).toFloat())
+        val data = Data(dx = endX - startX,
+            dy = endY - startY,
+            moveX = moveX,
+            moveY = moveY,
+            vx = xVelocity,
+            vy = yVelocity,
+            x0 = startX,
+            y0 = startY,
+            fingerArea = event.size,
+            pressure = event.pressure,
+            time = endTime - startTime)
+
+        if (isActionMove) {
+            touchData.add(data)
+            fillTouchSwipe()
+        } else {
+            touchSwipeData.add(data)
+            fillTouchData()
+        }
+        isActionMove = false
+        resetData()
+    }
+
+    private fun fillTouchData() {
+        val movement = Movement(time_start = dateFormat.format(startTime),
+            time_stop = dateFormat.format(endTime),
+            data = touchData,
+            phone_orientation = getDeviceOrientation())
+        tap.add(movement)
+        val jsonData = Gson().toJson(touchBody)
+        Log.d("json touch body is: ", jsonData)
+    }
+
+    private fun fillTouchSwipe() {
+        val movement = Movement(time_start = dateFormat.format(startTime),
+            time_stop = dateFormat.format(endTime),
+            data = touchSwipeData,
+            phone_orientation = getDeviceOrientation())
+        swipe.add(movement)
+        val jsonData = Gson().toJson(touchBody)
+        Log.d("json touch body is: ", jsonData)
+    }
+
+    private fun resetData() {
+        startX = 0f
+        endX = 0f
+        startY = 0f
+        endY = 0f
+        moveX = 0f
+        moveY = 0f
+        startTime = 0L
+        endTime = 0L
+    }
+
+    private fun calculateVelocity(
+        startDistance: Float,
+        endDistance: Float,
+        duration: Float,
+    ): Float {
+        val distance = abs(endDistance - startDistance)
+        return distance / duration
     }
 }
--- app/src/main/java/com/t2/motionsensors/domain/entity/SensorBody.kt
+++ app/src/main/java/com/t2/motionsensors/domain/entity/SensorBody.kt
@@ -4,10 +4,22 @@ data class SensorBody(
     var user_id: String = "${systemSecondTime()}",
     var accelerometer: List<Coordinates>? = null,
     var gyroscope: List<Coordinates>? = null,
-    var magnetometer: List<Coordinates>? = null
+    var magnetometer: List<Coordinates>? = null,
+    var deviceMotion: List<DeviceMotion>? = null
 )
+
 fun systemSecondTime(): Long {
     return System.currentTimeMillis()
 }
 
 data class Coordinates(val x: Float?, val y: Float?, val z: Float?, val time: String)
+data class CoordinatesDevice(val x: Float?, val y: Float?, val z: Float?)
+data class DeviceMotion(
+    var accelerationIncludingGravity: CoordinatesDevice? = null,
+    var rotation: Rotation? = null,
+    var acceleration: CoordinatesDevice? = null,
+    var orientation: Int = 0,
+    var time: String? = ""
+)
+
+data class Rotation(val gamma: Float?, val alpha: Float?, val beta: Float?)
