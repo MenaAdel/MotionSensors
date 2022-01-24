6f2c8842fb39ea429cfb08246bf7377b7a81aedc
Diala <d.budair@t2.sa>
2022-01-24 11:54:22 +0200

    adding default value for device details

--- app/src/main/java/com/t2/motionsensors/domain/entity/DeviceDetails.kt
+++ app/src/main/java/com/t2/motionsensors/domain/entity/DeviceDetails.kt
@@ -1,18 +1,18 @@
 package com.t2.motionsensors.domain.entity
 
 data class DeviceDetails(
-    val carrier: String,
-    val deviceId: String,
-    val userId: String,
-    val phoneOS: String,
-    val deviceType: String,
+    val carrier: String = "",
+    val deviceId: String = "",
+    val userId: String = "",
+    val phoneOS: String ="",
+    val deviceType: String = "",
     val screenSpecs: ScreenSpecs
 )
 
 data class ScreenSpecs(
-    val safeAreaPaddingTop: Int,
-    val safeAreaPaddingBottom: Int,
-    val width: Int,
-    val height: Int,
-    val diameter: Double
+    val safeAreaPaddingTop: Int = 0,
+    val safeAreaPaddingBottom: Int = 0,
+    val width: Int = 0,
+    val height: Int = 0,
+    val diameter: Double = 0.0
 )
